057da3520a4a977f83107c30f1dfb275d5aee83d
Gabriella <gabrielarichardes@gmail.com>
2025-09-23 12:29:01 -0300

    Aula3 (concerto de um erro da atv)

--- Exercicios/017.js
+++ Exercicios/017.js
@@ -1,23 +1,23 @@
 //Soma
 const somaArrow = (v1, v2) => {    
     console.log(v1 + v2); 
-}
+};
 somaArrow(16, 19);
 
 //Subtração
 const subtracaoArrow = (v1, v2) => {    
     console.log(v1 - v2); 
-}
+};
 subtracaoArrow(10, 5);
 
 //Divisão
 const divisaoArrow = (v1, v2) => {    
     console.log(v1 / v2); 
-}
+};
 divisaoArrow(40, 5);
 
 //Multiplicação
 const multiplicacaoArrow = (v1, v2) => {    
       console.log(v1 * v2); 
-}
+};
 multiplicacaoArrow(16, 19);
